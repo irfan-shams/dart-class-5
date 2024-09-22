69cdd364e9e7b8421a3db22f5bcf4bbb38630b4e
muzammil <muzammilhadiqa901@gmail.com>
2024-09-22 16:57:13 +0500

    Updated

--- function.dart
+++ function.dart
@@ -1,30 +1,56 @@
 void main() {
-  List students =[
-    {
-      "name": "", 
-      "rollNo": "", 
-      "subjects": {
-        "English": 50,
-        "Urdu": 90
-      },
-      "percentage": "", // after
-      "grade": "" // after
-    }    
-  ];
-  for (var student in students) {
-    var result = calculateStudentMarks(student);
-    student['percentage'] = result;
-  }
-  print(students);
+  // List students =[
+  //   {
+  //     "name": "", 
+  //     "rollNo": "", 
+  //     "subjects": {
+  //       "English": 50,
+  //       "Urdu": 90
+  //     },
+  //     "percentage": "", // after
+  //     "grade": "" // after
+  //   }    
+  // ];
+  // for (var student in students) {
+  //   var result = calculateStudentMarks(student);
+  //   student['percentage'] = result;
+  // }
+  // print(students);
+  print(abc(0, 0, 30));
+  // print(abcv2(eng: 20, urdu: ));
+  abcv2(maths: 30, eng: 90,);
 }
 
-calculateStudentMarks(student) {
-  int totalMarks = 200;
-  int obtainMarks = student['subjects']['English'] + student['subjects']['Urdu'];
-  num percentage = obtainMarks / totalMarks * 100;
-  print(percentage);
-  return percentage;
+abc([eng, urdu, maths]){
+  // print(eng);
+  // print(urdu);
+  // print(maths);
 }
+abcv2({int? eng, urdu = 0, required int maths}){
+  print("eng: $eng");
+  print("urdu: $urdu");
+  print("maths: $maths");
+}
+
+// positioned required parameter
+// positioned optional parameter
+// optional parameter | named parameter
+// required parameter | named parameter
+
+// calculateStudentMarks(student) {
+//   int totalMarks = 200;
+//   int obtainMarks = student['subjects']['English'] + student['subjects']['Urdu'];
+//   num percentage = obtainMarks / totalMarks * 100;
+//   if (percentage >= 90) {
+//     print("A+ Grade with $percentage");
+//   } else {
+//     print("A Grade with $percentage");
+//   }
+//   print(percentage);
+//   return percentage;
+// }
+
+
 
 // marksSheet() {
 //   var name = "Ali";
