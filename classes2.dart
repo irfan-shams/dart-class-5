b8847b98083fa0796df974546363e98e9a327a10
muzammil <muzammilhadiqa901@gmail.com>
2024-09-29 17:29:21 +0500

    Updated

--- classes2.dart
+++ classes2.dart
@@ -1,8 +1,31 @@
 void main() {
   // Objects | Instance
-  Student student1 = Student('Ahmed', 'RN001');
-  student1.printStudentIno();
+  // Student student1 = Student(name: 'Ahmed', rollNo: 'RN001');
+  // student1.printStudentIno();
+  // print("----------");
+  // Student studentSpecial1 = Student.special('Hilal', 'RN002');
+  // studentSpecial1.printStudentIno();
+
+  Cat cat1 = Cat();
+  cat1.printBlood('Dark Red','Black');
+  print("----------");
+  Cat cat2 = Cat();
+  cat2.printBlood('Light Red', 'White');
 }
+
+class Animal {
+  var blood = '';
+}
+
+class Cat extends Animal {
+  var skin = '';
+  printBlood(blood, skin) {
+    print('Blood Color: $blood');
+    print('Skin Color: $skin');
+  }
+}
+
+class Dog {}
 
 class Student {
   // Properties | Attributes
@@ -11,10 +34,21 @@ class Student {
   var age = '';
 
   // Default Constructor
-  Student(studentName, studentRollNo) {
-    name = studentName;
-    rollNo = studentRollNo;
-  }
+  // Student({required this.name, required this.rollNo});
+
+  // Named Constructor
+  // Student.special(this.name, this.rollNo) {}
+
+  // Default Constructor
+  // Student(name, rollNo) {
+  //   this.name = name;
+  //   this.rollNo = rollNo;
+  // }
+  
+  // Student(studentName, studentRollNo) {
+  //   name = studentName;
+  //   rollNo = studentRollNo;
+  // }
 
   // Methods | Behaviours
   // setStudentInfo(studentName, studentRollNo) {
@@ -28,4 +62,3 @@ class Student {
     print('Age: $age');    
   }
 }
-
